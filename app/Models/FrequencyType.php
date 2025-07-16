dc462ab20cb39eff40ec0a20c17a64b2e0cdac9b
SaherQaid <saherqaid2020@gmail.com>
2025-07-16 03:17:22 +0300

    feat(models): add FrequencyType model relationships, fillable, and factory 🔁

---
+++ app/Models/FrequencyType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FrequencyType extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'days_between',
+        'description',
+    ];
+
+    /**
+     * Get the plans for the frequency type.
+     */
+    public function plans()
+    {
+        return $this->hasMany(Plan::class);
+    }
+}
