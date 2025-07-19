053c35b8bc452cd1d5146f96d437120ee31dcd4c
SaherQaid <saherqaid2020@gmail.com>
2025-07-19 21:08:43 +0300

    feat(resource): Add TrackingResource for API tracking responses

---
+++ app/Http/Resources/TrackingDetailResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrackingDetailResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'trackingId' => $this->tracking_id,
+            'trackingTypeId' => $this->tracking_type_id,
+            'fromTrackingUnitId' => $this->from_tracking_unit_id,
+            'toTrackingUnitId' => $this->to_tracking_unit_id,
+            'actualAmount' => $this->actual_amount,
+            'comment' => $this->comment,
+            'score' => $this->score,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
+    }
+}
---
+++ app/Http/Resources/TrackingResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrackingResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'planId' => $this->plan_id,
+            'date' => $this->date,
+            'note' => $this->note,
+            'behaviorNote' => $this->behavior_note,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
+    }
+}
