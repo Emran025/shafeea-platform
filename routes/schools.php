b79c76c8173fd250d31c44055746608e7ef51a2b
SaherQaid <saherqaid2020@gmail.com>
2025-08-23 22:23:40 +0300

    feat(schools): Update school routes

--- routes/schools.php
+++ routes/schools.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Schools\SchoolController;
 use App\Http\Controllers\Schools\TeacherController;
 
 Route::prefix('schools')->name('schools.')->group(function () {
+    // School management routes
+    Route::resource('/', SchoolController::class)->parameters(['' => 'school']);
+    Route::get('{school}/analytics', [SchoolController::class, 'analytics'])->name('analytics');
+    
     // Resource routes for teachers within schools
     Route::resource('teachers', TeacherController::class);
 }); 
